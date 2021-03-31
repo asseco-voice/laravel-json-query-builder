5784285735e50082b3ac43c9b44eaddeb2ccacec
Marko Dupor <Norgul@users.noreply.github.com>
2021-03-31 12:21:39 +0200

    Cleanup (#26)
    * Cleanup
    
    * Lock update

--- src/CategorizedValues.php
+++ src/CategorizedValues.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Asseco\JsonQueryBuilder;
 
 use Asseco\JsonQueryBuilder\Config\TypesConfig;
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Asseco\JsonQueryBuilder\Tests;
 
 use Asseco\JsonQueryBuilder\JsonQueryServiceProvider;
