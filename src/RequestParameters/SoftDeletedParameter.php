2af7f4ba4a42e8901f436005f4bf68bca6be5791
jmilotic <josip.milotic@asseco-see.hr>
2021-06-02 09:44:31 +0200

    Implemented searching by soft deleted models

---
+++ src/RequestParameters/SoftDeletedParameter.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Asseco\JsonQueryBuilder\RequestParameters;
+
+use Asseco\JsonQueryBuilder\Exceptions\JsonQueryBuilderException;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SoftDeletedParameter extends AbstractParameter
+{
+    public static function getParameterName(): string
+    {
+        return 'soft_deleted';
+    }
+
+    protected function areArgumentsValid(): void
+    {
+        if (count($this->arguments) != 1) {
+            throw new JsonQueryBuilderException("Parameter '{$this->getParameterName()}' expects only one argument.");
+        }
+
+        if (!in_array($this->arguments[0], [1, '1', true, 'true'], true)) {
+            throw new JsonQueryBuilderException("Parameter '{$this->getParameterName()}' expects to be 'true' if it is to be used.");
+        }
+    }
+
+    protected function appendQuery(): void
+    {
+        $this->builder->withoutGlobalScope(SoftDeletingScope::class);
+    }
+}
