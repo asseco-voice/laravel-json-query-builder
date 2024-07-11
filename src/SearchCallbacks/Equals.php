393d83f7aa3cb01721365b2ed19ccea6676794da
Ngadhnjim Istrefi <hex494D49@users.noreply.github.com>
2024-07-11 08:57:33 +0200

    Vsliv30 3515 creation time filter misbehaviour (#57)
    * Extended the Equals operator for Date(Time) filtering
    
    * Apply fixes from StyleCI
    
    ---------
    
    Co-authored-by: Ngadhnjim Istrefi <ngadhnjim.istrefi@asseco-see.hr>
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/SearchCallbacks/Equals.php
+++ src/SearchCallbacks/Equals.php
@@ -42,9 +42,26 @@ class Equals extends AbstractCallback
         }
 
         if ($values->and) {
-            if ($this->isDate($this->searchParser->type) || $this->isDateTime($this->searchParser->type)) {
+            if ($this->isDate($this->searchParser->type)) {
                 foreach ($values->and as $andValue) {
                     $builder->orWhereDate($column, $andValue);
+                }
+            } elseif ($this->isDateTime($this->searchParser->type)) {
+                foreach ($values->and as $andValue) {
+                    $dateTimeValue = new \DateTime($andValue);
+                    $formattedDateTime = $dateTimeValue->format('Y-m-d H:i:s');
+
+                    if ($dateTimeValue->format('s') === '00') {
+                        $builder->orWhere(function ($query) use ($column, $formattedDateTime) {
+                            $query->whereDate($column, '=', date('Y-m-d', strtotime($formattedDateTime)))
+                                  ->whereTime($column, '>=', date('H:i', strtotime($formattedDateTime)))
+                                  ->whereTime($column, '<', date('H:i', strtotime($formattedDateTime . ' +1 minute')));
+                        });
+                    } else {
+                        $builder->orWhere(function ($query) use ($column, $formattedDateTime) {
+                            $query->where($column, '=', $formattedDateTime);
+                        });
+                    }
                 }
             } else {
                 $builder->whereIn($column, $values->and);
